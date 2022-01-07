38ece84cb774e4fb9c956024034e5424e69ff0e4
Arthur <newartroll11@gmail.com>
2022-01-07 11:38:04 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import styles from "./App.module.css";
 import { MyInp } from "./components/inputs/MyInp";
 import Text from "./components/inputs/Text";
@@ -6,21 +6,29 @@ import Text from "./components/inputs/Text";
 const App: React.FC = () => {
   const [val, setVal] = useState("");
   const [wearablesList, setWearablesList] = useState<string[]>([]);
+  const [clicked, setClicked] = useState(0);
+
   function adding(e: React.ChangeEvent<HTMLInputElement>) {
     setVal(e.target.value);
   }
-
   function newElems() {
     setWearablesList([...wearablesList, val]);
     setVal("");
+    setClicked(clicked + 1);
   }
+
+  // const addNew = useMemo(() => {
+  //   return newElems();
+  // }, [clicked]);
 
   return (
     <div className={styles.app}>
       <MyInp val={val} setVal={setVal} adding={adding} />
       <button onClick={newElems}>add</button>
       {wearablesList &&
-        wearablesList.map((el: string, i: number) => <Text key={i} element={el}/>)}
+        wearablesList.map((el: string, i: number) => (
+          <Text key={i} element={el} />
+        ))}
     </div>
   );
 };
